aa4f26b54474fe95576db2c6d0bc260e9d8dc6f4
Chosen2730 <70141158+Chosen2730@users.noreply.github.com>
2022-03-13 16:37:49 +0100

    Updates

---
+++ precache-manifest.4c1d7fb0f535b5ea28f1d0a8a27c2677.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96edadb46f32221cc5b0f7dbc512e0f5",
+    "url": "/Accordion/index.html"
+  },
+  {
+    "revision": "85c370830cd8199210db",
+    "url": "/Accordion/static/css/main.0ec4dadb.chunk.css"
+  },
+  {
+    "revision": "11e2b97d487a12b7b136",
+    "url": "/Accordion/static/js/2.f745c462.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Accordion/static/js/2.f745c462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c370830cd8199210db",
+    "url": "/Accordion/static/js/main.0d040e41.chunk.js"
+  },
+  {
+    "revision": "f28ff91942394798008c",
+    "url": "/Accordion/static/js/runtime-main.2665c421.js"
+  }
+]);
